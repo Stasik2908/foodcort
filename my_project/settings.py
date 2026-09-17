2064de71ca5c6c121cd8f997946d89b748d30757
Stasik2908 <likvidator988@gmail.com>
2026-09-17 10:32:36 +0300

    Fix settings debug false

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cnt+x^ib^fhm)+dw#w2%s4xi95&ew8mx^!@11!9f*rd^xr1b(0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".vercel.app",
